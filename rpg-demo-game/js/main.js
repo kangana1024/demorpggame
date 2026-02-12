646c5b7b8aebd010ab3eeb7e529b3e445929fde1
kanchana sondee <52461323+showkhun@users.noreply.github.com>
2026-02-12 14:27:47 +0700

    add reource

--- rpg-demo-game/js/main.js
+++ rpg-demo-game/js/main.js
@@ -1,23 +1,12 @@
 //=============================================================================
-// main.js
+// main.js v1.10.0
 //=============================================================================
 
-//-----------------------------------------------------------------------------
-/**
- * The static class that handles the window frame and browser environment.
- *
- * @namespace
- */
-function Utils() {
-    throw new Error("This is a static class");
-}
-
 // Initialize the game on page load
-window.onload = function() {
-    PluginManager.setup($plugins);
-    window.effekseer = effekseer;
-    effekseer.initRuntime(
-        "js/libs/effekseer.wasm",
-        () => SceneManager.run(Scene_Boot)
-    );
+window.onload = function () {
+  PluginManager.setup($plugins);
+  window.effekseer = effekseer;
+  effekseer.initRuntime("js/libs/effekseer.wasm", () =>
+    SceneManager.run(Scene_Boot),
+  );
 };
